8fc3d849eaa955606f60bcbd8a5630739f68a94d
Denis Vasilevsky <d.a.vasilevsky@gmail.com>
2016-06-05 21:44:32 +0300

    some scripts added

---
+++ TestSwiftRepo/build-purely.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while getopts ":c:" opt
+do
+  case $opt in
+    c)comment=$OPTARG
+      echo "-c was triggered!" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+## Archiving                                                                    
+echo "Archiving..."
+xcodebuild -workspace ./TestSwiftRepo.xcworkspace -scheme TestSwiftRepo archive -archivePath ./TestSwiftRepo.xcarchive
+## Creating ipa                                                                 
+echo "Creating IPA file..."
+
+xcodebuild  -exportArchive \
+            -exportOptionsPlist ./exportOptions.plist \
+            -archivePath ./TestSwiftRepo.xcarchive \
+            -exportPath ./TestSwiftRepo/Translate.ipa
+
+# sh ./Nitro/etc/testfairy-upload-ios.sh ./Nitro/etc/Translate.ipa/Translate.ipa
+rm -rf ./TestSwiftRepo.xcarchive
+# rm ./Nitro/etc/Translate.ipa/Translate.ipa
