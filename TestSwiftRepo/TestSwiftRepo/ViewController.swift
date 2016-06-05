973b32c97e3e5ed16d7872451ae0f9dcc8b49bce
Denis Vasilevsky <d.a.vasilevsky@gmail.com>
2016-06-05 19:46:14 +0300

    Initial commit

---
+++ TestSwiftRepo/TestSwiftRepo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestSwiftRepo
+//
+//  Created by denis.vasilevsky on 05.06.2016.
+//  Copyright © 2016 denis.vasilevsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Launched")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
